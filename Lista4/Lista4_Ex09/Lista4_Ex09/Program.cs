16bc0eaad0898efa541b18b2ad9e787914899aa7
Gabriel <99262377+GabrielG-tech@users.noreply.github.com>
2023-09-06 13:16:37 -0300

    Aula14-online

---
+++ Lista4/Lista4_Ex09/Lista4_Ex09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4_Ex09 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[,] matriz = new int[3, 4];
+
+            for (int i = 0; i < matriz.GetLength(0); i++) {
+                for (int j = 0; j < matriz.GetLength(1); j++) {
+                    
+                }
+            }
+        }
+        public static void ExibirMatriz(int[,] matriz) {
+            Console.WriteLine(matriz);
+        }
+    }
+}
