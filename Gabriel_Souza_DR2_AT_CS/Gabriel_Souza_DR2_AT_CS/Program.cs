126d92ddeb9e8b903d4f62e3176f116585e815da
GabrielG-tech <gabriel@mpsouza.com.br>
2023-09-29 12:22:38 -0300

    AT

--- Gabriel_Souza_DR2_AT_CS/Gabriel_Souza_DR2_AT_CS/Program.cs
+++ Gabriel_Souza_DR2_AT_CS/Gabriel_Souza_DR2_AT_CS/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* https://docs.google.com/document/d/1rpXTfLUYUmu3dqwO6F5dGMD_6TFgOoK13nDvsJeaulQ/edit */
 namespace Gabriel_Souza_DR2_AT_CS {
     internal class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine("Bora codar");
         }
     }
 }
