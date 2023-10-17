3ff9920f60d9edb87d9db0ff8bcdb0bc11f0a93a
GabrielG-tech <gabriel@mpsouza.com.br>
2023-10-17 10:55:31 -0300

    C#/II

---
+++ C# II/Aula01.1/Aula01.1/Conta.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01._1 {
+    internal class Conta {
+    }
+}
